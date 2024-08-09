08ae7f66959b71f3d57a30b7b8f0b9e20cdb988e
nic00la <nic00la.kaleta@gmail.com>
2024-08-09 22:59:38 +0200

    Creating Web API Project ✅

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -1,13 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
