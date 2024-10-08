6281db54857f8e5113ee44641c0b07069d70b43b
nic00la <nic00la.kaleta@gmail.com>
2024-10-08 18:45:08 +0200

    Fixed the problem with async adding new city 💪

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { City } from '../models/city';
 import { CityService } from '../services/city.service';
-import {ReactiveFormsModule, FormControl, FormGroup, Validators, FormArray} from '@angular/forms';
-import {DisableControlDirective} from "../directives/disable-control.directive";
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { DisableControlDirective } from "../directives/disable-control.directive";
 
 @Component({
   selector: 'app-cities',
@@ -32,7 +32,7 @@ export class CitiesComponent {
     });
   }
 
-  get putCityFormArray() : FormArray{
+  get putCityFormArray(): FormArray {
     return this.putCityForm.get("cities") as FormArray;
   }
 
@@ -77,6 +77,10 @@ export class CitiesComponent {
         next: (response: City) => {
           console.log(response);
           this.cities.push(new City(response.id, response.name));
+          this.putCityFormArray.push(new FormGroup({
+            id: new FormControl(response.id, [Validators.required]),
+            name: new FormControl({ value: response.name, disabled: true }, [Validators.required])
+          }));
           this.postCityForm.reset();
         },
         error: (error: any) => {
@@ -85,13 +89,14 @@ export class CitiesComponent {
       });
     }
   }
-  // Executes when the clicks on 'Edit' button for the particular city
-  editClicked(city : City) : void {
+
+  // Executes when the user clicks on 'Edit' button for the particular city
+  editClicked(city: City): void {
     this.editCityId = city.id ?? null;
   }
 
-  // Executes when the clicks on 'Update' button after editing the city name
-  updateClicked(i : number) : void {
+  // Executes when the user clicks on 'Update' button after editing the city name
+  updateClicked(i: number): void {
     this.citiesService.putCity(this.putCityFormArray.controls[i].value).subscribe({
       next: (response: string) => {
         console.log(response);
@@ -104,6 +109,6 @@ export class CitiesComponent {
         console.log(error);
       },
       complete: () => {}
-    })
+    });
   }
 }
