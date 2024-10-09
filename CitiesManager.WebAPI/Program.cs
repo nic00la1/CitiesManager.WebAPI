ad0dfca7e821010678408d0025e81915f1397f3c
nic00la <nic00la.kaleta@gmail.com>
2024-10-09 14:55:51 +0200

    Identity with web API

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -1,4 +1,7 @@
+using CitiesManager.Core.Identity;
 using CitiesManager.Infrastructure.DatabaseContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
@@ -71,6 +74,21 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.Password.RequiredLength = 5;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireDigit = true;
+    }).AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders()
+    .AddUserStore<UserStore<ApplicationUser, ApplicationRole,
+        ApplicationDbContext, Guid>>()
+    .AddRoleStore<RoleStore<ApplicationRole, ApplicationDbContext, Guid>>();
+
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -88,6 +106,7 @@ app.UseSwaggerUI(options =>
 app.UseRouting();
 app.UseCors();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
